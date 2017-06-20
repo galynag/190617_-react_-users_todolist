2a78b6612523465ecc71890cb3ca37993b3a02a5
galynag <g.alenicheva@gmail.com>
2017-06-20 22:19:31 +0300

    v0.6 load next blocks was added

--- app/src/BlocksItem.js
+++ app/src/BlocksItem.js
@@ -6,7 +6,7 @@ import React, {Component} from 'react';
 export default class BlocksItem extends Component {
     render() {
         return(
-            <div className={`answer ${this.props.data}`}>
+            <div className={`answer${this.props.data}`}>
                 <div className="box" >1</div>
                 <div className="box">1</div>
                 <div className="box">1</div>
--- app/src/LoadMoreBlocks.js
+++ app/src/LoadMoreBlocks.js
@@ -5,32 +5,29 @@ import React,{Component} from 'react';
 import BlocksItem from './BlocksItem';
 
 export default class LoadMoreBlocks extends Component {
-    state = {
-        classBox1:'none',
-        classBox2:'none',
-        classBox3:'none',
-        classBox4:'none',
-        classBox5:'none'
-    }
+
+    state ={
+        counterBox: 1
+    };
     loadMore = () => {
-        this.setState({counterBox:this.state.counterBox+5})
-    }
+        if (this.state.counterBox<=5)  this.setState({counterBox:++this.state.counterBox})
+        else return false;
+    };
     render() {
         return(
             <div>
                 <h2 className="left">Задание 7: </h2>
                 <p className="left">При нажатии на кнопку Load сделать добавление квадратиков на страницу.</p>
-                <BlocksItem />
-                <BlocksItem data={this.state.classBox1} />
-                <BlocksItem data={this.state.classBox2} />
-                <BlocksItem data={this.state.classBox3} />
-                <BlocksItem data={this.state.classBox4} />
-                <BlocksItem data={this.state.classBox5} />
-
+                <BlocksItem  data=""/>
+                <BlocksItem  data={(this.state.counterBox >=2) ? "":" none"} />
+                <BlocksItem  data={(this.state.counterBox >=3) ? "":" none"}/>
+                <BlocksItem  data={(this.state.counterBox >=4) ? "":" none"}/>
+                <BlocksItem  data={(this.state.counterBox >=5) ? "":" none"}/>
+                <BlocksItem  data={(this.state.counterBox >=6) ? "":" none"}/>
                 <button
                     onClick={this.loadMore}
                 >Load More</button>
             </div>
         )
     }
-}
+};
